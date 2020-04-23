b73c2ed1a0c8b4a6c97283e1d893a6826b123e53
Noah Day <sheltielover432@gmail.com>
2020-04-22 20:29:14 -0400

    Base Class

--- Advanced_CPP/CH01_01.cpp
+++ Advanced_CPP/CH01_01.cpp
@@ -2,19 +2,49 @@
 //
 
 #include <iostream>
+#include <sstream>
+using namespace std;
 
+class Employee {
+private:
+    string name;
+    double pay;
+public:
+    Employee()
+    {
+        name = "";
+        pay = 0;
+    }
+    Employee(string empName, double payrate)
+    {
+        name = empName;
+        payrate = payrate;
+    }
+    string GetName()
+    {
+        return name;
+    }
+    double GetPay()
+    {
+        return pay;
+    }
+    void setPay(double payRate)
+    {
+        pay = payRate;
+    }
+    string toString()
+    {
+        stringstream stm;
+        stm << name << ":" << pay;
+        return stm.str();
+    }
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    Employee emp1("Jane Smith", 250000);
+    Employee emp2("Bill Brown", 450000);
+    cout << emp1.toString() << endl;
+    cout << emp2.toString() << endl;
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
